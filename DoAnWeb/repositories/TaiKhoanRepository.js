1cb05f5e16d389bf98ef52c82e328bdb507fe4a2
Nguyễn Khánh Hòa <nguyenkhanhhoalk@gmail.com>
2019-06-18 23:34:06 +0700

    đăng nhập, đăng ký user default role độc giả

--- DoAnWeb/repositories/TaiKhoanRepository.js
+++ DoAnWeb/repositories/TaiKhoanRepository.js
@@ -8,25 +8,31 @@ exports.createBaiDang = baiDang => {
     return data.save(sql);
 }
 
-exports.login = user =>{
+function login1(username, password) {
+    var sql = "select * from nguoi_dung where username =\"" + username + "\" and PASSWORD = \"" + password + "\"";
+    return data.load(sql);
+}
+module.exports.login1 = login1;
+
+exports.login = user => {
     var sql = `select * from nguoi_dung where username = '${user.userName}' and password = '${user.passWord}'`;
     return data.load(sql);
 }
 
-exports.checkAccountExist = username =>{
-	var sql = `select count(*) as total from nguoi_dung where username = '${username}' `;
-	return data.load(sql);
+exports.checkAccountExist = username => {
+    var sql = `select count(*) as total from nguoi_dung where username = '${username}' `;
+    return data.load(sql);
 }
 
 exports.add = user => {
     var sql = `insert into nguoi_dung(chuc_vu_id, username, password, ten_nguoi_dung, bi_danh, ngay_sinh,so_dien_thoai, email, dia_chi, tinh_trang) 
     values(${user.chuc_vu}, '${user.username}','${user.password}', '${user.ten_nguoi_dung}', '${user.bi_danh}', '${user.ngay_sinh}','${user.so_dien_thoai}','${user.email}', '${user.dia_chi}',${user.tinh_trang}); `;
-	return data.save(sql);
+    return data.save(sql);
 }
 
 /* ------------------------------- DOI MAT KHAU -------------------------*/
 exports.changePassword = user => {
     var sql = `update nguoi_dung set password = '${user.password}' `;
-	return data.load(sql);
+    return data.load(sql);
 }
 
